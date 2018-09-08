964fde1e44d1cd3de5f5baff77899a435df29077
lisalouaeh <lisa.lou.aeh@gmail.com>
2018-09-08 21:11:46 +0200

    ajout dossier script JS pour le popUp

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+function redirectionX(){
+    if (confirm('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')){
+        location.href =  'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    }
+};
+//les 10secondes sont exprimées en millisecondes
+setTimeout(redirectionX, 10000);
